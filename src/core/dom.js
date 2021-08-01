ea6f99032ca2ddbb40e3f14dfd48fe56240bc982
ibadoo <138006683@qq.com>
2021-08-01 18:45:52 +0800

    优化弹幕创建性能

--- src/core/dom.js
+++ src/core/dom.js
@@ -18,6 +18,7 @@ class BulletContainer {
     if (!BulletContainer.initStyletate) {
       // 如果没有创建过样式，进行样式初始化
       initStyle(hashKey);
+      // 修改状态为已初始化
       BulletContainer.initStyletate = true;
     }
 
@@ -38,7 +39,7 @@ class BulletContainer {
    * @Author: SAM
    * @Description: 将弹幕挂载到容器
    * @Date: 2021-08-01 02:41:57
-   * @param {element} parentDom 容器节点
+   * @param {HTMLElement} parentDom 容器节点
    * @return {this} 当前实例
    */
   mounted(parentDom) {
@@ -66,9 +67,6 @@ class BulletContainer {
    */
   getOffAreaWidth() {
     return document.body.offsetWidth - this.dom.offsetWidth;
-    //   const { offsetWidth, offsetHeight } = this.container;
-    //   // 计算区域外的宽度，用于弹幕起始位置
-    //   this.offAreaWidth = document.body.offsetWidth - offsetWidth;
   }
 
   /**
@@ -105,10 +103,7 @@ class BulletContainer {
  * @Date: 2021-08-01 03:12:05
  */
 class Bullet {
-  constructor(payload, hashKey, params) {
-    // 记录弹幕数据
-    this.payload = payload;
-
+  constructor(hashKey) {
     // 创建弹幕节点
     const domBullet = document.createElement('div');
     // 增加弹幕class
@@ -119,24 +114,88 @@ class Bullet {
     // 名称
     const domNickName = document.createElement('span');
     domNickName.className = 'nickName';
-    domNickName.innerText = payload.nickName + '：';
 
     // 内容
     const domContent = document.createElement('span');
     domContent.className = 'content';
-    domContent.innerText = payload.content;
+
+    // 插入弹幕
+    domBullet.appendChild(domNickName);
+    domBullet.appendChild(domContent);
+
+    // 记录唯一key
+    this.key = hashKey;
+
+    // 把节点记录起来做为模板使用
+    this.template = domBullet;
+
+    // 定义一个克隆方法
+    this.template.clone = function () {
+      const dom = this.cloneNode(true);
+      dom.domNickName = dom.querySelector('.nickName');
+      dom.domContent = dom.querySelector('.content');
+      Object.defineProperty(dom, 'nickName', {
+        enumerable: true,
+        configurable: true,
+        get() {
+          return this.domNickName.innerText;
+        },
+        set(newVal) {
+          this.domNickName.innerText = newVal;
+          return this;
+        },
+      });
+
+      Object.defineProperty(dom, 'content', {
+        enumerable: true,
+        configurable: true,
+        get() {
+          return dom.domContent.innerText;
+        },
+        set(newVal) {
+          this.domContent.innerText = newVal;
+          return this;
+        },
+      });
+
+      return dom;
+    };
+  }
+
+  /**
+   * @Author: SAM
+   * @Description: 创建一个弹幕
+   * @Date: 2021-08-01 17:16:44
+   * @param {object} payload 弹幕数据
+   * @param {object} params 弹幕等相关参数
+   * @return {HTMLElement} dom对象
+   */
+  create(payload, params) {
+    // 从模板克隆节点
+    const domBullet = this.template.clone();
+
+    // 设置名称
+    domBullet.nickName = payload.nickName + '：';
+    // 设置内容
+    domBullet.content = payload.content;
 
     // 上下距离
     domBullet.style.top = params.top + 'px';
     // 弹幕时间（决定弹幕速度）
     domBullet.style.animationDuration = params.dt + 's';
     // 区域外宽度（决定起始位置）
-    domBullet.style.setProperty(`--bulletStart-${hashKey}`, `calc(100vw - ${params.offAreaWidth}px)`);
+    domBullet.style.setProperty(`--bulletStart-${this.key}`, `calc(100vw - ${params.offAreaWidth}px)`);
 
-    // 插件弹幕
-    domBullet.appendChild(domNickName);
-    domBullet.appendChild(domContent);
-    this.dom = domBullet;
+    return {
+      // 记录弹幕数据
+      payload,
+      // 记录dom节点
+      dom: domBullet,
+      // 绑定on方法
+      on: this.on,
+      // 绑定remove方法
+      remove: this.remove,
+    };
   }
 
   /**
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const { BulletContainer, Bullet } = require('./core/dom');
  * @return {number} 随机整数
  */
 module.exports = class BulletChat {
-  constructor(parentDom) {
+  constructor(parentDom, { spacing = 50, speed = 12 } = {}) {
     if (!parentDom) {
       throw new Error('需要传入一个容器节点');
     }
@@ -20,21 +20,23 @@ module.exports = class BulletChat {
     // 唯一key
     this.hashKey = 'data-v-bulletChat';
 
-    // 区域外宽度
-    this.offAreaWidth = 0;
-
-    // 弹幕容器高度
-    this.containerHight = 0;
-    // 弹幕间隔
-    this.spacing = 50;
-    // 弹幕速度
-    this.speed = 12;
-
     // 记录容器节点
     this.parentDom = parentDom;
+    // 弹幕间隔
+    this.spacing = spacing;
+    // 弹幕速度
+    this.speed = speed;
+
+    // 区域外宽度
+    this.offAreaWidth = 0;
+    // 弹幕容器高度
+    this.containerHight = 0;
 
     // 当前弹幕容器
     this.bulletContainer = new BulletContainer(this.hashKey).mounted(parentDom);
+
+    // 弹幕调度中心
+    this.bulletControl = new Bullet(this.hashKey);
 
     // 窗口尺寸变化时，进行重新计算区域外宽度
     window.addEventListener('resize', this.setOffAreaWidth.bind(this));
@@ -52,13 +54,13 @@ module.exports = class BulletChat {
 
   // 添加一个弹幕实时
   add(payload) {
-    const params = this.createBulletParams(payload);
-    const bullet = new Bullet(payload, this.hashKey, params);
+    const params = this.createBulletParams();
+    const bullet = this.bulletControl.create(payload, params);
     this.bulletContainer.add(bullet);
   }
 
   // 设置弹幕位置
-  createBulletParams(payload) {
+  createBulletParams() {
     // 随机时间差距
     const dt = Math.random();
 
